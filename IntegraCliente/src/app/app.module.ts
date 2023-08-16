dbaa43b13fbd20a3d8441496019ec52960d54ae6
Everton Leão <eleao@avipam.com.br>
2023-08-16 16:09:08 -0300

    Cliente Layout login e cadastro

--- IntegraCliente/src/app/app.module.ts
+++ IntegraCliente/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
